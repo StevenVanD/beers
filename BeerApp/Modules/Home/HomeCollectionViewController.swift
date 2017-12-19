3d1a7245364e704f46ef59abb3d339a32fa5f02a
Steven Van Durm <stevenvandurm@macbook-pro-van-steven.local>
2017-12-19 11:54:45 +0100

    Feature1: CollectionView Added UIIlage and loading of the images

--- BeerApp/Modules/Home/HomeCollectionViewController.swift
+++ BeerApp/Modules/Home/HomeCollectionViewController.swift
@@ -12,8 +12,10 @@ class HomeCollectionViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     var collectionData = ["1 yay", "2 yay", "3 yay", "4 yay", "5 yay", "6 yay"]
+    
     public var viewModel: HomeViewModel = HomeViewModel()
-
+    public var service: Service = Service()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,22 +23,71 @@ class HomeCollectionViewController: UIViewController {
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout // swiftlint:disable:this force_cast
         let width = (view.frame.width - (amountOfRows) * layout.minimumInteritemSpacing) / amountOfRows
         layout.itemSize = CGSize(width: width, height: width)
+        
+        reloadData()
+        viewModel.breweriesUpdateHandler = { [unowned self] in
+            DispatchQueue.main.async {
+                self.reloadUI()
+            }
+        }
+        viewModel.beersUpdateHandler = { [unowned self] in
+            DispatchQueue.main.async {
+                self.reloadUI()
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    func reloadUI() {
+        self.viewModel.setClosestBrewery()
+        //stopt hier vroegtijdig mee
+        DispatchQueue.main.async { [unowned self] in
+            //self.brewNameLabel.text = self.viewModel.closestBrewName
+            //self.brewAddressLabel.text = self.viewModel.closestBrewAddress
+            self.collectionView.reloadData()
+            
+        }
+    }
+    
+    func reloadData() {
+        service.getBeers { [unowned self] (error, beers) in
+            if let error = error {
+                print("error \(error.localizedDescription)")
+            }
+            
+            if let beers = beers {
+                DispatchQueue.main.async {
+                    self.viewModel.upDateBeerList(beerList: beers, for: 1)
+                }
+                self.reloadUI()
+            }
+        }
+    }
 }
 
 extension HomeCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return collectionData.count
+        guard let beers = viewModel.beers, let breweries = viewModel.breweries else {
+            return 0
+        }
+        return beers.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
-        if let label = cell.viewWithTag(100) as? UILabel {
-            label.text = collectionData[indexPath.row]
+        guard let beers = viewModel.beers, let breweries = viewModel.breweries else {
+            return cell
+        }
+        let selectedBeer = beers[indexPath.row]
+        let selectedBeerPhotoURL = selectedBeer.photoURL
+        
+        if let image = cell.viewWithTag(100) as? UIImageView {
+            func downloadImage(url: URL) {
+                image.sd_setImage(with: url, placeholderImage: UIImage(named: "beer.png"))
+            }
+            downloadImage(url: selectedBeerPhotoURL)
         }
         return cell
     }
