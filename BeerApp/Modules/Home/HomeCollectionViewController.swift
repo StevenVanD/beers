317aefaecdf1fbc3d7313a0983aff4a43509fd74
Steven Van Durm <stevenvandurm@macbook-pro-van-steven.local>
2017-12-19 10:02:40 +0100

    Feature 1: Collection view Set up code with a label added basic Xib for a custom cell

--- BeerApp/Modules/Home/HomeCollectionViewController.swift
+++ BeerApp/Modules/Home/HomeCollectionViewController.swift
@@ -8,6 +8,35 @@
 
 import UIKit
 
-class HomeCollectionViewController: UICollectionViewFlowLayout {
+class HomeCollectionViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var collectionData = ["1 yay", "2 yay", "3 yay", "4 yay", "5 yay", "6 yay"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let width = view.frame.size.width / 2
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout // swiftlint:disable:this force_cast
+        layout.itemSize = CGSize(width: width, height: width)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
 
+extension HomeCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        if let label = cell.viewWithTag(100) as? UILabel {
+            label.text = collectionData[indexPath.row]
+        }
+        return cell
+    }
+    
 }
